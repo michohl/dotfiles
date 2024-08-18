465dd935d7a788045766a7c04035e7b94bfff36c
Michael Riesberg-Timmer <MichaelRT@pallid.dev>
2024-08-18 14:51:13 -0500

    feat(wezterm): Disable bell and add familiar tab switching

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,14 +7,32 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 config.keys = {
+  -- Stop new windows from appearing when using Neovim auto completion
   {
     key = 'n',
     mods = 'CTRL|SHIFT',
     action = wezterm.action.DisableDefaultAssignment,
   },
+  -- Switch tabs
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+
 }
 
+-- Wayland isn't currently supported which is what I use in Fedora 40
 -- https://github.com/wez/wezterm/issues/4962#issuecomment-2122189202
 config.enable_wayland = false
+
+-- Disable the terminal from making noises on typos
+config.audible_bell = "Disabled"
+
 
 return config
